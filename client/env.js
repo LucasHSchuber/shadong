a7c3cecea4072737de007401e64a36b3ba40a8d3
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2024-10-16 07:21:33 +0200

    Remove env.js and .env from repository

--- client/env.js
+++
@@ -1,9 +0,0 @@
-// env.js
-
-const keys = {
-    API_KEY: "2e93d183ba52d94429b3e362f5c3efbe",
-    SPOTIFY_CLIENT_ID: '1a624a77bfb243d5a504f4227522ab2b',
-    SPOTIFY_CLIENT_SECRET: 'b8d9c783a9e74a58b4853f3cc382a162',
-}
-
-export default keys
